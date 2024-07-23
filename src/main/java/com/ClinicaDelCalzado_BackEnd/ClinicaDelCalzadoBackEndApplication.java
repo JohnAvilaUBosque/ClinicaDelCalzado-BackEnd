ebc5fdc16f29beddcb27546ef430a47d8598aab7
marielenarb <mbarriosr@unbosque.edu.co>
2024-07-22 20:25:14 -0500

    Base spring boot scaffolding

---
+++ src/main/java/com/ClinicaDelCalzado_BackEnd/ClinicaDelCalzadoBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ClinicaDelCalzado_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaDelCalzadoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaDelCalzadoBackEndApplication.class, args);
+	}
+
+}
