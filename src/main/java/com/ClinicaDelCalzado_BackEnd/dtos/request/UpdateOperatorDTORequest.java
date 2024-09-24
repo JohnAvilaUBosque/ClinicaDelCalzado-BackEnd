6984512d61ee7cfd5854f3afc5a78daa17dd9428
marielenarb <camilobernal@MacBook-Pro-de-Camilo.local>
2024-09-23 22:34:33 -0500

    Added crud operators

---
+++ src/main/java/com/ClinicaDelCalzado_BackEnd/dtos/request/UpdateOperatorDTORequest.java
@@ -0,0 +1,17 @@
+package com.ClinicaDelCalzado_BackEnd.dtos.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class UpdateOperatorDTORequest {
+
+    private String operatorName;
+    private String opePhoneNumber;
+}
